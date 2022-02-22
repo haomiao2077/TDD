8a8775c787959be2fae2763fa372b20e1b786a0b
haomiao <zhanghm1994@gmail.com>
2022-02-22 21:07:48 +0800

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -4,11 +4,18 @@ import unittest
 import time
 
 class NewVisitorTest(unittest.TestCase):
+
     def setUp(self) -> None:
         self.browser = webdriver.Edge()
     
     def tearDown(self) -> None:
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn("row_text", [row.text for row in rows])
+
     
     def test_can_start_a_list_and_retriver_it_later(self):
         self.browser.get("http://localhost:8000")
@@ -26,15 +33,15 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+
         inputbox = self.browser.find_element_by_id("id_new_item") 
-        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys("2: Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
-        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
         self.fail("Finish the test!")
     
 if __name__ == '__main__':

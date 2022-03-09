338fdf353bbdf2c79f32026dd7846b13a0b6b9d6
haomiao <zhanghm1994@gmail.com>
2022-03-09 20:10:01 +0800

    Split out unit test into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -15,43 +15,6 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-    # def test_redirects_after_POST(self):
-    #     response = self.client.post("/lists/new", data={'item_text': 'A new list item'})
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-
-class ListAndItemModelsTest(TestCase):
-
-    def test_saving_and_retireving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
-
 
 class ListViewTest(TestCase):
 

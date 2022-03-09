2a06d7a2c2f1a1f3fae5d49d694879b863d7782c
haomiao <zhanghm1994@gmail.com>
2022-03-09 22:11:12 +0800

    adjust new list view to do model validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,8 +1,31 @@
-from .base import FunctionalTest
 from unittest import skip
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from .base import FunctionalTest
 
 
 class ItemValidationTest(FunctionalTest):
-    @skip
+
     def test_cannot_and_empty_list_items(self):
-        pass
+        self.browser.get(self.live_server_url)
+        self.browser.find_element(value="id_new_item").send_keys(Keys.ENTER)
+
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.find_element(By.CSS_SELECTOR, '.has_error').text,
+                                     "You can't have an empty list item")
+        )
+
+        self.browser.find_element(value='id_new_item').send_keys('Buy milk')
+        self.browser.find_element(value='id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        self.browser.find_element(value='id_new_item').send_keys(Keys.ENTER)
+        self.wait_for(lambda: self.assertEqual(self.browser.find_element(By.CSS_SELECTOR, '.has_error').text,
+                                               "You can't have an empty list item"))
+        self.browser.find_element(value='id_new_item').send_keys('Make tea')
+        self.browser.find_element(value='id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
+        self.fail('finish this test!')
